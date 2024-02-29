e5fc83b106e85ebd4f3e124433b3d3ac7be557b2
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-02-29 19:08:12 -0300

    Atualizado

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,18 +3,22 @@ import styles from './Form.module.css'
 import FormLogo from '../../assets/form-logo.png'
 export default function Form() {
   return (
-    <form>
-      <img src={FormLogo} alt="imagem-logo" />
+    <form className={styles.form}>
+      <img src={FormLogo} alt="imagem-logo" className={styles.img}/>
       <label>
+        Nome
         <input type="text" placeholder='Nome Completo' />
       </label>
       <label>
+        E-mail
         <input type="text" placeholder='E-mail' />
       </label>
       <label>
+        Senha
         <input type="text" placeholder='Senha' />
       </label>
       <label>
+        Confirmar senha
         <input type="text" placeholder='Confirmar senha' />
       </label>
       <button>Enviar</button>
